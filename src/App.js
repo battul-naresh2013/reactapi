8ed1a1d159c91bb08851263d611554acba9fb789
Naresh Battul <naresh.battul@capgemini.com>
2022-10-25 16:23:02 +0530

    react rest api get request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itmes: [],
+      isLoading: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users') // fetch call
+      .then(res => res.json()) // get parsed json data
+      .then(jsonData => { //set to state
+        this.setState({
+          isLoading: true,
+          itmes: jsonData
+        })
+      })
+  }
+
+  render() {
+    const margin = { margin: 0 };
+    const list = { borderBottom: "1px red solid", padding: "5px" };
+    const { isLoading, itmes } = this.state;
+
+    if (!isLoading) {
+      return <div>Loading ..... </div>
+    } else {
+      return (
+        <div className="my-class">
+          <ul>
+            {itmes.map((item) =>
+              <li style={list} key={item.id}>Name: {item.name}
+                <p style={margin}>Email: {item.email}</p>
+              </li>
+            )}
+          </ul>
+        </div >
+      );
+    }
+  }
 }
 
 export default App;
